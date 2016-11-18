42c9c11f0c4fb8674090ab305d5eb0b54e559757
Julia Connelly <juliakconnelly@gmail.com>
2016-11-18 10:03:00 +0100

    commiting broken code before I make major changes

--- environment/searchclient/HeuristicUtil.java
+++ environment/searchclient/HeuristicUtil.java
@@ -69,4 +69,81 @@ public class HeuristicUtil {
         return minDist;
     }
 
+    public static int[][] makeAPSP() {
+        int[][] distances = new int[Node.MAX_ROW][Node.MAX_COL];
+        for(int v = 0; v < Node.MAX_COL * Node.MAX_ROW; i++) {
+            distances[v][v] = 0;
+        }
+
+    }
+
+    private int[][] initDistanceMatrix() {
+        int numVertices = Node.MAX_ROW * Node.MAX_COL;
+        int[][] distances = new int[numVertices][numVertices];
+        for(int u = 0; u < numVertices; u++) {
+            for(int v = 0; v < numVertices; v++) {
+                int[] uLocCoords = idxToCoords(u, Node.MAX_COL);
+                int[] vLocCoords = idkToCoords(v, Node.MAX_COL);
+                if(u == v) {
+                    distance[u][v] = 0;
+                }
+                if(cellIsNotWall(r + 1, c)) {
+                    distance[locIdx][coordsToIdx(r + 1, c, Node.MAX_COL)] = 1;
+                }else {
+                    distance[locIdx][coordsToIdx(r + 1, c, Node.MAX_COL)] = Integer.MAX_VALUE;
+                }
+                if(cellIsNotWall(r - 1, c)) {
+                    distance[locIdx][coordsToIdx(r - 1, c, Node.MAX_COL)] = 1;
+                }else {
+                    distance[locIdx][coordsToIdx(r - 1, c, Node.MAX_COL)] = Integer.MAX_VALUE;
+                }
+                if(cellIsNotWall(r, c + 1)) {
+                    distance[locIdx][coordsToIdx(r, c + 1, Node.MAX_COL)] = 1;
+                }else {
+                    distance[locIdx][coordsToIdx(r, c + 1, Node.MAX_COL)] = Integer.MAX_VALUE;
+                }
+                if(cellIsNotWall(r, c - 1)) {
+                    distance[locIdx][coordsToIdx(r, c - 1, Node.MAX_COL)] = 1;
+                }else {
+                    distance[locIdx][coordsToIdx(r, c - 1, Node.MAX_COL)] = Integer.MAX_VALUE;
+                }
+            }
+        }
+
+
+
+        HashMap<Integer, LinkedList<Integer>> adjacencyList = new HashMap<Integer, LinkedList<Integer>>();
+        for(int r = 0; r < Node.MAX_ROW; r++) {
+            for(int c = 0; c < Node.MAX_COL; c++) {
+                LinkedList<Integer> adjacentNodes = new LinkedList<Integer>();
+                if(cellIsNotWall(r + 1, c)) {
+                    adjacentNodes.add(coordsToIdx(r + 1, c, Node.MAX_COL));
+                }
+                if(cellIsNotWall(r - 1, c)) {
+                    adjacentNodes.add(coordsToIdx(r - 1, c, Node.MAX_COL));
+                }
+                if(cellIsNotWall(r, c + 1)) {
+                    adjacentNodes.add(coordsToIdx(r, c + 1, Node.MAX_COL));
+                }
+                if(cellIsNotWall(r, c - 1)) {
+                    adjacentNodes.add(coordsToIdx(r, c - 1, Node.MAX_COL));
+                }
+                adjacencyList.put(coordsToIdx(r, c, Node.MAX_COL), adjacentNodes);
+            }
+        }
+        return adjacencyList;
+    }
+
+    private boolean cellIsNotWall(int r, int c) {
+        if(r >= Node.MAX_ROW || r < 0 || c < 0 || c >= Node.MAX_COL) {
+            return false;
+        }
+        return !Node.walls[r][c];
+    }
+
+    private boolean cellsAreAdjacent(int r1, int c1, int r2, int c2) {
+        if(r1 == r2 && Math.abs(c1 - c2) == 1) return true;
+        if(c1 == c2 && Math.abs(r1 - r2) == 1) return true;
+        return false;
+    }
 }
